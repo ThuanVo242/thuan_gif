267fe846c3433e720372e4881f5dda6a59dcdaaf
= <=>
2021-06-14 22:55:40 +0700

    Update my project

--- RaceDay.js
+++ RaceDay.js
@@ -1,14 +1,14 @@
 let raceNumber = Math.floor(Math.random() * 1000);
-let status = soon;
+let status = 'soon';
 let age = 15;
 
 if (age === 20 && status === soon) {
     raceNumber += 1000;
 }
 
-if (age > 18 && status === soon) {
+if (age > 18 && status === 'soon') {
     console.log(`Race number : ${raceNumber} runner start 9:30am`);
-} else if (age > 18 && status === late) {
+} else if (age > 18 && status === 'late') {
     console.log(`Race number : ${raceNumber} runner start 11:30am`);
 } else if (age < 18) {
     console.log(`Race number : ${raceNumber} runner start 12:30am`);
